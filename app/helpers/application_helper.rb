9166438f0634e08965254dd7ad806316a1097908
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-01-19 13:38:27 +0900

    google books api モジュールの修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  module GoogleBooksApi
+    def jason_from_url(url)
+      JSON.parse(Net::HTTP.get(URI.parse(Addressable::URI.encode(url))))
+    end
+  
+    def url_from_keyword(keyword)
+      "https://www.googleapis.com/books/v1/volumes?q=#{keyword}&country=JP&maxResults=10"
+    end
+  end
 end
--- lib/google_books_api.rb
+++
@@ -1,9 +0,0 @@
-module GoogleBooksApi
-  def jason_from_url(url)
-    JSON.parse(Net::HTTP.get(URI.parse(Addressable::URI.encode(url))))
-  end
-
-  def url_from_keyword
-    "https://www.googleapis.com/books/v1/volumes?q=#{keyword}&country=JP&maxResults=10"
-  end
-end
