09c2113fdec916e71c3a697705c5d4c2312feacb
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-01-20 18:17:46 +0900

    検索機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,9 @@
 class BooksController < ApplicationController
   def index
-    
   end
 
   def search
-    
+    books = GoogleBooks.search("#{params[:keyword]}", {:count => 30}).to_a
+    @books = Kaminari.paginate_array(books).page(params[:page]).per(10)
   end
 end
